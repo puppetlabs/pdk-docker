70ad7a9e0d23c8c677dbe5c5684e63d1f9e4c12a
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2024-12-19 22:51:55 +0000

    Create bigbird7821/pdk:2.7.0.0_cd4pe for cd4pe

--- install-pdk-release.sh
+++ install-pdk-release.sh
@@ -3,12 +3,6 @@
 set +x
 
 source pdk-release.env
-# if uname -m = aarch64, then use arm64, otherwise use amd64
-if [ "$(uname -m)" = "aarch64" ]; then
-  PDK_DEB_URL="${PDK_DEB_URL_ARM64}"
-else
-  PDK_DEB_URL="${PDK_DEB_URL_AMD64}"
-fi
 
 curl --fail -L -o \
   "pdk.deb" \
